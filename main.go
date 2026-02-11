d5caabfcb3417e5ec5865ea4a1b828f68a552f22
rezafauzan <rezafauzan593@gmail.com>
2026-02-11 13:13:00 +0700

    Feat: add struct to access 'Tech Academy' in obj.str[3][1][2].man[0].tech.academy

--- main.go
+++ main.go
@@ -16,7 +16,21 @@ type world struct{
 	world string
 }
 
+type academy struct{
+	academy string
+}
 
+type tech struct{
+	tech academy
+}
+
+type man struct{
+	man [1]tech
+}
+
+type obj struct{
+	str [4][2][3]man
+}
 
 func main() {
 	var we are = are{
@@ -31,6 +45,29 @@ func main() {
 		world: "Hello World",
 	}
 
+	var obj obj= obj{
+		str: [4][2][3] man{
+			{
+				{{},{},{}},{{},{},{}},
+			},
+			{
+				{{},{},{}},{{},{},{}},
+			},
+			{
+				{{},{},{}},{{},{},{}},
+			},
+			{
+				{{},{},{}},
+				{
+					{},
+					{},
+					man{[1]tech{tech{academy{academy: "Tech Academy"}}}},
+				},
+			},
+		},
+	}
+
 	fmt.Println(we.are.the.best)
 	fmt.Println(hello.world)
+	fmt.Println(obj.str[3][1][2].man[0].tech.academy)
 }
