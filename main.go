6226c59235d4fbe3023144f5637cefd1e961ef23
rezafauzan <rezafauzan593@gmail.com>
2026-02-11 11:35:48 +0700

    Feat: add struct to access 'Koda' in we.are.the.best

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+type best struct {
+	best string
+}
+type the struct {
+	the best
+}
+type are struct {
+	are the
+}
+type we struct {
+	we are
+}
+
+func main() {
+	var we are = are{
+		are: the{
+			the: best{
+				best: "Koda",
+			},
+		},
+	}
+
+	fmt.Println(we.are.the.best)
 }
